b532e2fe8ddd7f531afca302747a4c239a0d373d
Aroize <gipermonk@bk.ru>
2020-06-19 03:02:03 +0300

    moved reindexing and sampling to static methods. also removed flooding and indexing from init

--- ncf/dataset.py
+++ ncf/dataset.py
@@ -15,38 +15,16 @@ All parameters can be tuned via config
 class UserItemDataset(BaseDataset):
     def __init__(self, data_dict, config):
         self.config = config
-        user_mapper = None
-        item_mapper = None
-        self.user_features = None
-        self.item_features = None
-        
-        if not config.is_reindexed:
-            data, user_mapper, item_mapper = reindex_data(data_dict['samples'])
-            self.user_mapper = user_mapper
-            self.item_mapper = item_mapper
-            self.data = data
-        else:
-            self.data = data_dict['samples']
-        
-        if config.flood_negative:
-            negative_samples = flood_negative(self.data, config)
-            #positive data should be provided with no ratings, because in NCF
-            #we mark interaction as 1 if user knows about item's existence
-            self.data = list(map(lambda x: (x[0], x[1], 1.0), self.data))
-            #otherwise, we mark non-observed items as zero
-            self.data.extend(negative_samples)
-            
+        self.data = data_dict['samples']
         if 'user_features' in data_dict:
-            if user_mapper is not None:
-                self.user_features = reindex_features(data['user_features'], user_mapper)
-            else:
-                self.user_features = data['user_features']
+            self.user_features = data['user_features']
+        else:
+            self.user_features = None
         
         if 'item_features' in data_dict:
-            if item_mapper is not None:
-                self.item_features = reindex_features(data['item_features'], item_mapper)
-            else:
-                self.item_features = data['item_features']
+            self.item_features = data['item_features']
+        else:
+            self.item_features = None
 
 
     def __len__(self):
@@ -67,7 +45,62 @@ class UserItemDataset(BaseDataset):
         return sample
     
     
+    """
+    Data reindexing was moved to class, because if you create negative
+    samples when init data, you can accidentally miss indicies or samples, which
+    contains in test split
+    So, you should use #reindex_data and #flood_negative, before train/test split.
+    It will ensure, that no data was leaked and all users and items were mapped to
+    new indicies
+    Also, method returns both data and mappers, so reindexing user and item features
+    indicies is user's responsibility, because features are too general for trying
+    to do it with no information about its format.
+    """
+    @staticmethod
+    def reindex_data(data_samples):
+        user_mapper = {}
+        item_mapper = {}
+        reindexed_data = []
+        for user, item in data_samples:
+            uid = insert(user, user_mapper)
+            iid = insert(item, item_mapper)
+            reindexed_data.append((uid, iid))
+        return (reindexed_data, user_mapper, item_mapper)
     
+    
+    @staticmethod
+    def flood_negative(positive_data_samples, config):
+        per_user = config.negatives_per_user
+        #For fast search we need to create set of tuples like (user, item)
+        positive_set = set(map(lambda x: (x[0], x[1]), positive_data_samples))
+        #To iterate over each user once (and don't search in already created negative samples)
+        user_set = set(map(lambda x: x[0], positive_data_samples))
+
+        #Cause items have been already reindexed, MAX_ITEM_ID = len(set(ITEMS)) - 1
+        item_next_idx = len(set(map(lambda x: x[1], positive_data_samples)))
+        negative_samples = []
+        for user in user_set:
+            sample = set()
+            while len(sample) != per_user:
+                item_idx = np.random.randint(0, item_next_idx)
+                pair = (user, item_idx)
+                if pair not in sample and pair not in positive_set:
+                    sample.add(pair)
+            #Generate samples with zero labels
+            negative_samples.extend(list(sample))
+        return negative_samples
+
+    
+    @staticmethod
+    def union_samples(positive, negative):
+        data = []
+        positive = list(map(lambda x: (*x, 1.0), positive))
+        negative = list(map(lambda x: (*x, 0.0), negative))
+        data.extend(positive)
+        data.extend(negative)
+        return data
+
+
 def insert(idx, idx_dict):
     if idx not in idx_dict:
         new_idx = len(idx_dict)
@@ -75,40 +108,7 @@ def insert(idx, idx_dict):
     else:
         new_idx = idx_dict[idx]
     return new_idx
-        
-        
 
-def reindex_data(data_samples):
-    user_mapper = {}
-    item_mapper = {}
-    reindexed_data = []
-    for user, item in data_samples:
-        uid = insert(user, user_mapper)
-        iid = insert(item, item_mapper)
-        reindexed_data.append((uid, iid))
-    return (reindexed_data, user_mapper, item_mapper)
-
-
-def flood_negative(positive_data_samples, config):
-    per_user = config.negatives_per_user
-    #For fast search we need to create set of tuples like (user, item)
-    positive_set = set(map(lambda x: (x[0], x[1]), positive_data_samples))
-    #To iterate over each user once (and don't search in already created negative samples)
-    user_set = set(map(lambda x: x[0], positive_data_samples))
-    
-    #Cause items have been already reindexed, MAX_ITEM_ID = len(set(ITEMS)) - 1
-    item_next_idx = len(set(map(lambda x: x[1], positive_data_samples)))
-    negative_samples = []
-    for user in user_set:
-        sample = set()
-        while len(sample) != per_user:
-            item_idx = np.random.randint(0, item_next_idx)
-            pair = (user, item_idx)
-            if pair not in sample and pair not in positive_set:
-                sample.add(pair)
-        #Generate samples with zero labels
-        negative_samples.extend(list(map(lambda x: (x[0], x[1], 0.0) ,sample)))
-    return negative_samples
     
 if __name__ == "__main__":
     
@@ -116,15 +116,18 @@ if __name__ == "__main__":
     from torch.utils.data.dataloader import DataLoader
     import sys
     
-    config = Hparam(sys.argv[1])
+    config = Hparam(sys.argv[1]).dataset
     data = [[420, 606], [420, 101], [69, 101], [69, 202]]
+    reindexed_data, user_mapper, item_mapper = UserItemDataset.reindex_data(data)
+    negative_samples = UserItemDataset.flood_negative(reindexed_data, config)
+    data = UserItemDataset.union_samples(reindexed_data, negative_samples)
     data_dict = { "samples" : data }
     
-    dataset = UserItemDataset(data_dict, config.dataset)
+    dataset = UserItemDataset(data_dict, config)
     dataloader = DataLoader(dataset, batch_size=2, shuffle=True)
     
-    print('to convert user indicies back: ', dataset.user_mapper)
-    print('to convert item indicies back: ', dataset.item_mapper)
+    print('converted user ids ', user_mapper)
+    print('converted item ids ', item_mapper)
     # Ensure, that dataset supports batches
     for _, data in enumerate(dataloader, 0):
         print(data)
